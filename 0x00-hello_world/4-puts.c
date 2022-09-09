855735de3a3af5a1bb8d95f8d9a4f70db253dbcd
LowellUfot <lowellufot@gmail.com>
2022-09-08 21:08:43 -0700

    modified 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * This is a C program that prints exactly: 
+ * main: This is a C program that prints exactly: 
  * "Programming is like building a multilingual puzzle"
+ * return: (0) if it exits properly, non-zero if otherwise
  */
 int main(void)
 {
