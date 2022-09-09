24bce268a14cbecc87d6f267a0f13acdddfae62a
LowellUfot <lowellufot@gmail.com>
2022-09-08 22:16:23 -0700

    created script 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main - prints phrase
- * "with proper grammar, but the outcome is a piece of art,"
- * return - 0 if exits properly, non-zero if otherwise
+ * main - entry point
+ *
+ * Return: 0 if success, non-zero if otherwise
  */
+
 int main(void)
-{	
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
