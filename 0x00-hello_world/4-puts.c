51865ea4b060736446e5febc412cb8df489f3098
LowellUfot <lowellufot@gmail.com>
2022-09-08 21:03:17 -0700

    modified 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle");
+#include <stdio.h>
+/**
+ * This is a C program that prints exactly: 
+ * "Programming is like building a multilingual puzzle"
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
