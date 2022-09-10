d273d6535cc81b306dcfaf18718fe811932becb4
LowellUfot <lowellufot@gmail.com>
2022-09-10 15:26:54 -0700

    created script 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - program starts here
+ *
+ * Return: 1 if sucess. 0 otherwise
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar(c + '0');
+
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
