e780adf24afb63558b840a2eb0bd22ea832a7a03
LowellUfot <lowellufot@gmail.com>
2022-09-09 21:48:19 -0700

    created script 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program starts here
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'a'; c--)
+	{
+		putchar(c);
+	}
+	return (0);
+}
