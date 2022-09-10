be47ca6ff9b0d95a52c931e19c73cb3ce4fd94e9
LowellUfot <lowellufot@gmail.com>
2022-09-09 21:00:32 -0700

    created script 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program starts here
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ *
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
