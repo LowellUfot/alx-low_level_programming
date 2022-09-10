66c5fb3aeaa0504fcd8c5dc2957f3cf1715b3447
LowellUfot <lowellufot@gmail.com>
2022-09-10 15:55:14 -0700

    created script 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.io>
+
+/**
+ * main - program starts here
+ *
+ * Return: 0 if sucess. 1 if otherwise
+ *
+ */
+
+int main(void)
+{
+	int c;
+	int d;
+
+	for (c = 0; c <= 9; c++)
+	{
+		for (d = 0; d <= 9; d++)
+		{
+			if (c != d && c < d)
+			{
+				putchar (c + '0');
+				putchar (d + '0');
+				putchar (',');
+				putchar (' ');
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
