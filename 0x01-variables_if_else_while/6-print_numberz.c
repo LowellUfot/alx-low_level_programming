8ac164bfdaf75ff5bdcd62aa8485781a6347ddc2
LowellUfot <lowellufot@gmail.com>
2022-09-09 21:23:10 -0700

    created script 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program starts here
+ * Return: 1 if the number is positive. 0 otherwise
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
