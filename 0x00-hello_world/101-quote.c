27f62cfc5125a284579b8d98e9e7fd9366a87011
LowellUfot <lowellufot@gmail.com>
2022-09-12 15:47:57 -0700

    created script 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Program starts here
+ *
+ * Return: 1 if success, 0 if otherwise
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
