c9a6d9e6493da90d13bdb87fbc9373dc0befee40
LowellUfot <lowellufot@gmail.com>
2022-09-13 16:59:38 -0700

    created script 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of number
+ * @n: integer argument
+ *
+ * Return: 1 for +ve, 0 for 0, -1 for -ve
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
