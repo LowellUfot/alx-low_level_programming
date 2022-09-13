7eb6485ed7eb3c75bb6c6e1a8d12197252a2b46b
LowellUfot <lowellufot@gmail.com>
2022-09-13 12:22:58 -0700

    created script 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints 10x alphabets
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		print_alphabet();
+	}
+}
