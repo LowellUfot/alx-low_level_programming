e8898290da0e3b5f16f9eef16d877f11c26bb74a
LowellUfot <lowellufot@gmail.com>
2022-09-13 16:21:28 -0700

    created script 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower.c - prints lowercase character
+ *
+ * Return: 1 if successful, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (islower(c) == 1)
+	{
+		Return (1);
+	}
+	else
+	{
+		Return (0);
+	}
+}
