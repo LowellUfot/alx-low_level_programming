bc53715ee990c77ebb4153b72623b5e77afe74ad
LowellUfot <lowellufot@gmail.com>
2022-09-13 20:32:09 -0700

    created script 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 times table
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int prod;
+
+	for (i = 0; 1 < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			prod = (i * j);
+
+			if (j == 0)
+			{
+				_putchar (prod + '0');
+			}
+			else if (prod < 10)
+			{
+				_putchar (',');
+				_putchar (' ');
+				_putchar (' ');
+				_putchar (prod + '0');
+			}
+			else if (prod >= 10)
+			{
+				_putchar (',');
+				_putchar (' ');
+				_putchar ((prod / 10) + '0');
+				_putchar ((prod % 10) + '0');
+			}
+		}
+		_putchar ('\n');
+	}
+}
