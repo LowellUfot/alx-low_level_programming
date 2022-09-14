2fcb19d082af978e5d163831c7337a617ee5bd8a
LowellUfot <lowellufot@gmail.com>
2022-09-13 17:18:54 -0700

    created script 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - returns absolute value
+ * @j: int value to return abs
+ *
+ * Return: non zero j
+ */
+
+int _abs(int j)
+{
+	int x = abs(j);
+
+	return (x);
+}
