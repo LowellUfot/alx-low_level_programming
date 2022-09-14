ccc3f7501f7f7ab50cdcc2234c1b82780bfcb898
LowellUfot <lowellufot@gmail.com>
2022-09-13 17:26:22 -0700

    created script 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @j: int digit
+ *
+ * Return: last digit of j
+ */
+
+int print_last_digit(int j)
+{
+	int x;
+
+	x = j % 10;
+	return (x);
+}
