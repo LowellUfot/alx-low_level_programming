3fac762cb9c2b3594055a7b226a07cc2232cb8a0
LowellUfot <lowellufot@gmail.com>
2022-09-13 20:52:28 -0700

    created script 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print from int n to 98
+ * @n: start of number count
+ *
+ * Return: 0 always
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			_putchar (i + '0');
+		}
+	}
+	else if (n == 98)
+	{
+		_putchar (n + '0');
+	}
+	else
+	{
+		for (i = n; i <= 98; i--)
+		{
+			_putchar (i + '0')
+		}
+	}
+	_putchar ('\n');
+}
