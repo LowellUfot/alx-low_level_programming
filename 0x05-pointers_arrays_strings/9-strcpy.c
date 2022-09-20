52a661d33a978cd45b0745a3f58971e2bed876f2
LowellUfot <lowellufot@gmail.com>
2022-09-19 20:37:43 -0700

    created function 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	return (l);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by des
+ * @dest: copy source to this buffer
+ * @src: this is the source to copy
+ *
+ * Return: copy of original source
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int v;
+
+	for (v = 0; v <= _strlen(src); v++)
+	{
+		dest[v] = src[v];
+	}
+	return (dest);
+}
