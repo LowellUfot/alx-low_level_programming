cd43ffd5b91c28559ccfbab76f6228c693630229
LowellUfot <lowellufot@gmail.com>
2022-09-19 20:05:44 -0700

    created function 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	return (l);
+}
+
+/**
+ * puts2 - prints every second character of a string and new line
+ * @str: string to print
+ *
+ * Return: void
+ */
+
+
+void puts2(char *str)
+{
+	int b;
+
+	for (b = 0; str[b] != '\0' && b < _strlen(str); += 2)
+	{
+		_putchar(str[b]);
+	}
+	_putchar('\n');
+}
