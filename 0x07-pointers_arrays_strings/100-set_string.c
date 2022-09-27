55b962c58aa49dbed18b30baf72256b115cb1cef
LowellUfot <lowellufot@gmail.com>
2022-09-27 03:48:49 -0700

    created function 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to char
+ * @s: double pointer char
+ * @t: pointer char to be replaced
+ *
+ * Return: no return
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
