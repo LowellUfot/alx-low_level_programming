5a3d6aae79f1b95a977a82f550dda5ea945073c8
LowellUfot <lowellufot@gmail.com>
2022-09-26 19:57:19 -0700

    created function 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string s
+ * @c: character to locate in s
+ *
+ * Return: pointer to first c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+		{
+			return ((s + i));
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return ('\0');
+}
