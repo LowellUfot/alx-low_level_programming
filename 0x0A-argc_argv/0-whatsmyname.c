097abedb793e76485521fbccbee7ae7030da225c
LowellUfot <lowellufot@gmail.com>
2022-09-30 21:08:55 -0700

    created 1-args.c and 2-args.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - prints its name, followed by a new line.
+ * main - prints its name, followed by new line
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
 
-int main(int argc_attribute_((unused)), char *argv[])
+int main(int argc _attribute_((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print each argument passed to program on a line
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+
+	return (0);
+}
