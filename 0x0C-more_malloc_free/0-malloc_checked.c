10fb244565992a08592dc58b9ebd48613a98c890
LowellUfot <lowellufot@gmail.com>
2022-10-07 16:33:50 -0700

    created function 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of unit of bytes to allocate
+ *
+ * Return: pointer to allocated memory
+ * exits with status 98 if process fails
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *var;
+
+	var = malloc(sizeof(var) * b); /*allocate memory*/
+
+	/*validate malloc return*/
+	if (var == NULL)
+		exit(98);
+
+	return (var);
+}
