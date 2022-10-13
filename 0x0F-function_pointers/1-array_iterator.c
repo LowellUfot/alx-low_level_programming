4d951f70fef8bd10247673e540a45d9dc0f5057d
LowellUfot <lowellufot@gmail.com>
2022-10-12 20:32:08 -0700

    modified 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array != '\0' && action != '\0')
+	if (array && action)
 		for (i = 0; i < size; i++)
 			action(array[i]);
 }
