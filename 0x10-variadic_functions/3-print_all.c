e8484f5beac49bb89bc25953c8b8115c1004a3ab
LowellUfot <lowellufot@gmail.com>
2022-10-14 01:40:20 -0700

    created function 3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - prints anything
+ * @format - print format
+ *
+ */
+
+void print_all(const char * const format, ...)
+{
+	unsigned int i = 0;
+	char *str;
+	char *sep = "";
+	va_list any_args;
+
+	va_start(any_args, format);
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", sep, va_arg(any_args, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(any_args, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(any_args, double));
+				break;
+			case 's':
+				str = va_arg(any_args, char *);
+				if (!str)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s%s", sep, str);
+				break;
+		}
+		sep = ", ";
+		i++;
+	}
+	printf("\n");
+	va_end(any_args);
+}
