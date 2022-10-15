dbe605f4eada65be084b519e05c3b72e378a782a
LowellUfot <lowellufot@gmail.com>
2022-10-15 00:18:53 -0700

    modified 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,6 +13,7 @@ void print_all(const char * const format, ...)
 	unsigned int i = 0;
 	char *str;
 	char *sep = "";
+	int flag = 0;
 	va_list any_args;
 
 	va_start(any_args, format);
@@ -23,24 +24,30 @@ void print_all(const char * const format, ...)
 		{
 			case 'c':
 				printf("%s%c", sep, va_arg(any_args, int));
+				flag = 1;
 				break;
 			case 'i':
 				printf("%s%d", sep, va_arg(any_args, int));
+				flag = 1;
 				break;
 			case 'f':
 				printf("%s%f", sep, va_arg(any_args, double));
+				flag = 1;
 				break;
 			case 's':
 				str = va_arg(any_args, char *);
 				if (!str)
 				{
 					printf("%s(nil)", sep);
+					flag = 1;
 					break;
 				}
 				printf("%s%s", sep, str);
+				flag = 1;
 				break;
 		}
-		sep = ", ";
+		if (flag == 1)
+			sep = ", ";
 		i++;
 	}
 	printf("\n");
