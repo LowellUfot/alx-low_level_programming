b03c13b8f61f1536ea5a93d26610bbcb9d36fc7d
LowellUfot <lowellufot@gmail.com>
2022-10-16 03:59:43 -0700

    created dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory is for the project 0x0E. C - Structures, typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct called dog with name, owmner, and age members
+ * @name: dog name member
+ * @owner: dog owner member
+ * @age: dog age member
+ *
+ * Description: struct for dog
+ */
+
+struct dog
+{
+	char *owner;
+	char *name;
+	float age;
+};
