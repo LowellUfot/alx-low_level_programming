7a38e0c7d80273ab4a7a4393d3aebd8150099570
LowellUfot <lowellufot@gmail.com>
2022-10-20 19:22:23 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * Return: none
  */
 
-void _attribute_ ((constructor)) print_story()
+void print_story(void) __attribute__((constructor))
 {
 		printf("You're beat! and yet, you must allow");
 		printf(",\nI bore my house upon my back!\n");
