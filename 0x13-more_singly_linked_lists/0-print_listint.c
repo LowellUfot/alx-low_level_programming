efe05687772080b2e5bd2ef5eec87983273efc26
LowellUfot <lowellufot@gmail.com>
2022-10-25 16:49:41 -0700

    created function 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_listint - prints all elements of a linked list
+ * @h: pointer to linked list
+ *
+ * Return: prints to standard output and
+ * returns the number of nodes in the list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This directory contains function for the project: 0x13. C - More singly linked lists
