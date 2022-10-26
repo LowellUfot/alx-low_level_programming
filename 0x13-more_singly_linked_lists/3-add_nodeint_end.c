617fe7f7698a70b969c96008b68d0a62ac89ff21
LowellUfot <lowellufot@gmail.com>
2022-10-25 17:41:17 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -34,7 +34,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		{
 			current_node = current_node->next;
 		}
-		end_node->next = current_node->next;
 		current_node->next = end_node;
 	}
 	return (end_node);
