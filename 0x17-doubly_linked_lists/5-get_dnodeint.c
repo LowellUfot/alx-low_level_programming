f3b5b5b71288330301a39739e7eec0fc512991a0
LowellUfot <lowellufot@gmail.com>
2022-12-08 15:44:45 -0800

    5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns nth node of a linked list
+ * @head: pointer to head node
+ * @index: index of node to return
+ *
+ * Return: node at specified index n
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp;
+	unsigned int count = 0;
+
+	temp = malloc(sizeof(dlistint_t));
+
+	if (head == NULL)
+		return ('\0');
+
+	temp = head;
+
+	while (temp->next != NULL && (count < index))
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (temp);
+}
