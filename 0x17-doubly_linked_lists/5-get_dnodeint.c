455fcba569e5eb964911c013165a41ae06a24e72
LowellUfot <lowellufot@gmail.com>
2022-12-08 15:46:32 -0800

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	temp = head;
 
-	while (temp->next != NULL && (count < index))
+	while (temp != NULL && (count < index))
 	{
 		count++;
 		temp = temp->next;
