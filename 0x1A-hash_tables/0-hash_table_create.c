7928909d2d6601de886805e3585a48e29598ddf8
LowellUfot <lowellufot@gmail.com>
2023-01-05 17:38:19 -0800

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: NULL if error occurs, pointer to new hash table otherwise
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned long int i;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return ('\0');
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+		return ('\0');
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+
+	return (table);
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+This directory contains code for the project: 0x1A. C - Hash tables
