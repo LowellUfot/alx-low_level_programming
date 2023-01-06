0eb2c9445cb85c765695702f6dce944929b84b41
LowellUfot <lowellufot@gmail.com>
2023-01-06 03:33:12 -0800

    100-sorted_hash_table.c

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -21,16 +21,16 @@ shash_table_t *shash_table_create(unsigned long int size)
 
 	table = malloc(sizeof(shash_table_t));
 	if (table == NULL)
-		return (NULL)
+		return (NULL);
 
 	table->size = size;
 	table->array = malloc(sizeof(shash_node_t *) * size);
-	if (table->array[i] == NULL)
+	if (table->array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 	table->shead = NULL;
-	tanle->stail = NULL;
+	table->stail = NULL;
 
 	return (table);
 }
