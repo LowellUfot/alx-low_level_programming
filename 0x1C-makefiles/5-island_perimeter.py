8dafb2b49080b61ebf58dbda6a8ac91e7897871b
LowellUfot <lowellufot@gmail.com>
2023-02-27 06:56:18 -0800

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module for island perimeter calculation"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island in a grid
+    The grid represents water by 0 and land by 1
+
+    Args:
+        grid(list): a list of list of integers representing an island
+    Returns:
+        The perimeter of the island defined in grid
+    """
+
+    row_len = len(grid[0])
+    col_len = len(grid)
+    edges = 0
+    count = 0
+
+    for i in range(col_len):
+        for j in range(row_len):
+            if grid[i][j] == 1:
+                count ++
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges ++
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges ++
+    return (count * 4 - edges * 2)
